60eb8da688baef19c29ee8a8dd3196f6a33a4355
XRuiz9 <xdruiz92@gmail.com>
2019-07-12 11:11:02 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-// Add your code here
+fetch("http://localhost:3000/dogs", {
+	method: "POST",
+	headers: {
+		"Content-Type": "application/json",
+		"Accept": "application/json"
+	},
+	body: JSON.stringify({
+		dogName: "Byron",
+		dogBreed: "Poodle"
+	})
+})
+
+function submitData(name, email){
+	return fetch("http://localhost:3000/users", {
+		method: "POST",
+		headers: {
+			"Content-Type": "application/json",
+			"Accept": "application/json"
+		},
+		body: JSON.stringify({
+			name: name,
+			email: email
+		})
+	}).then(response => response.json())
+	.then(data => {
+		document.querySelector("body").innerHTML += data.id
+	})
+	.catch(function(error) {
+		document.querySelector("body").innerHTML += error.message
+	})
+}
