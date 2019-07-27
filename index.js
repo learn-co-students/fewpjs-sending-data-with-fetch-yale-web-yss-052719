67ac9883c8f7baadd8c3a572def9069d589c2628
jqzheng617 <jenna.zheng617@gmail.com>
2019-07-26 23:43:40 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Add your code here
+function submitData(name, email) {
+  return fetch("http://localhost:3000/users", {
+    method: "POST",
+    headers: {
+      'Content-Type': "application/json",
+      Accept: "application/json"
+    },
+    body: JSON.stringify({
+      name: name,
+      email: email
+    })
+  })
+  .then(res => res.json())
+  .then(data => {
+    document.querySelector('body').innerHTML += data.id
+  })
+  .catch(function(error) {
+    document.querySelector('body').innerHTML += error.message
+  })
+}
