f4412796f34a417af62585b885435a7624fc5ee3
MatthewP01 <matthew.peoples@yale.edu>
2019-07-27 16:10:41 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Add your code here
+function submitData(name, email){
+
+  const docBody = document.querySelector("body")
+
+  return fetch("http://localhost:3000/users", {
+		method: "POST",
+		headers: {
+			"Content-Type": "application/json",
+			"Accept": "application/json"
+		},
+		body: JSON.stringify({
+			name: name,
+			email: email
+		})
+	}).then(res => res.json())
+	.then(data => {
+		docBody.innerHTML += data.id
+	})
+	.catch(function(error) {
+		docBody.innerHTML += error.message
+	})
+}
